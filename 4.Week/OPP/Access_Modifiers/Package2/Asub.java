01cc0ab7da90d90196e404d1fe86073ae794a8f7
Yavuz Yaman <47952149+YavuzYaman217@users.noreply.github.com>
2024-11-05 22:35:07 +0300

    Add files via upload

---
+++ 4.Week/OPP/Access_Modifiers/Package2/Asub.java
@@ -0,0 +1,9 @@
+package Package2;
+import Package.*;
+
+public class Asub extends A {
+    public static void main(String[] args) {
+       Asub a = new Asub();
+       System.out.println(a.proctectedMassage2);
+    }
+}
---
+++ 4.Week/OPP/Access_Modifiers/Package2/C.java
@@ -0,0 +1,7 @@
+package Package2;
+import Package.*;
+
+public class C {
+    String defaultMassage = "This is the default";
+    public String publicMassage3 = "This is the public";
+}
